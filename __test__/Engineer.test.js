5c4e827a847f156db525649d29f1f4213282bfef
Michelle Smith <smith.michelle.a@gmail.com>
2021-02-10 09:49:39 -0500

    added Employee test

--- __test__/Engineer.test.js
+++ __test__/Engineer.test.js
@@ -1,1 +1,7 @@
+const Employee = require("./Employee");
 
+describe("Employee", () => {
+  it("should return all the employee name, id, email", () => {
+    expect(Employee).toBe(this.name, this.id, this.email);
+  });
+});
